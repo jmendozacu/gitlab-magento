6f63c1514eded67d0136205882141d371afea7ec
Ralph Smith <rsmith@astralbrands.com>
2018-11-02 09:35:04 -0400

    logic update

--- app/code/local/Astral/Shipping/Model/Observer.php
+++ app/code/local/Astral/Shipping/Model/Observer.php
@@ -3,26 +3,34 @@ class Astral_Shipping_Model_Observer{
     public function hideShippingMethods( Varien_Event_Observer $observer ){
             if (Mage::getDesign()->getArea() ===   Mage_Core_Model_App_Area::AREA_FRONTEND){
             $quote              = $observer->getEvent()->getQuote();
-            $address = $quote->getShippingAddress();
+            $address            = $quote->getShippingAddress();
             $store              = Mage::app()->getStore($quote->getStoreId());
             $carriers           = Mage::getStoreConfig('carriers', $store);
             $hiddenMethodCode   = 'freeshipping';
             $cc = $quote->getCouponCode();
             $sfs = false;
-                if(isset($cc)&&!empty($cc)) {
-                    $oCoupon = Mage::getModel('salesrule/coupon')->load($quote->getCouponCode(), 'code');
-                    $oRule = Mage::getModel('salesrule/rule')->load($oCoupon->getRuleId());
-                    Mage::log(__METHOD__." CouponCode ".$quote->getCouponCode());
-                    Mage::log(__METHOD__." simple_free_shipping ".$oRule->getData('simple_free_shipping'));
-                    $sfs = $oRule->getData('simple_free_shipping');
-                        if (!isset($sfs) || $sfs == 0) {
-                            foreach ($carriers as $carrierCode => $carrierConfig){
-                                if( $carrierCode ==  $hiddenMethodCode ){
-                                    $store->setConfig("carriers/{$carrierCode}/active", '0');
-                                }
-                            }
-                        }
+                if(!isset($cc)||empty($cc)) {
+                $this->hideFreeShipping($carriers);
+                }elseif(isset($cc)&&!empty($cc)){
+                $oCoupon = Mage::getModel('salesrule/coupon')->load($quote->getCouponCode(), 'code');
+                $oRule = Mage::getModel('salesrule/rule')->load($oCoupon->getRuleId());
+                Mage::log(__METHOD__." CouponCode ".$quote->getCouponCode());
+                Mage::log(__METHOD__." simple_free_shipping ".$oRule->getData('simple_free_shipping'));
+                $sfs = $oRule->getData('simple_free_shipping');
+                    if ($sfs == 0) {
+                        $this->hideFreeShipping($carriers);
+                    }
                 }
             }
     }
+
+    public function hideFreeShipping($carriers){
+        $hiddenMethodCode = 'freeshipping';
+            foreach ($carriers as $carrierCode => $carrierConfig){
+                if( $carrierCode ==  $hiddenMethodCode ){
+                $store->setConfig("carriers/{$carrierCode}/active", '0');
+                }
+            }
+    }
+
 }
