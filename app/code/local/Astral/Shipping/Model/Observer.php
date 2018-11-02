69ee346e3cce08c1ab1efe9e32cf49718e24ccdb
Ralph Smith <rsmith@astralbrands.com>
2018-11-02 09:14:17 -0400

    New shipping monitor

---
+++ app/code/local/Astral/Shipping/Model/Observer.php
@@ -0,0 +1,28 @@
+<?php
+class Astral_Shipping_Model_Observer{
+    public function hideShippingMethods( Varien_Event_Observer $observer ){
+            if (Mage::getDesign()->getArea() ===   Mage_Core_Model_App_Area::AREA_FRONTEND){
+            $quote              = $observer->getEvent()->getQuote();
+            $address = $quote->getShippingAddress();
+            $store              = Mage::app()->getStore($quote->getStoreId());
+            $carriers           = Mage::getStoreConfig('carriers', $store);
+            $hiddenMethodCode   = 'freeshipping';
+            $cc = $quote->getCouponCode();
+            $sfs = false;
+                if(isset($cc)&&!empty($cc)) {
+                    $oCoupon = Mage::getModel('salesrule/coupon')->load($quote->getCouponCode(), 'code');
+                    $oRule = Mage::getModel('salesrule/rule')->load($oCoupon->getRuleId());
+                    Mage::log(__METHOD__." CouponCode ".$quote->getCouponCode());
+                    Mage::log(__METHOD__." simple_free_shipping ".$oRule->getData('simple_free_shipping'));
+                    $sfs = $oRule->getData('simple_free_shipping');
+                        if (!isset($sfs) || $sfs == 0) {
+                            foreach ($carriers as $carrierCode => $carrierConfig){
+                                if( $carrierCode ==  $hiddenMethodCode ){
+                                    $store->setConfig("carriers/{$carrierCode}/active", '0');
+                                }
+                            }
+                        }
+                }
+            }
+    }
+}
