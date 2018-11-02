72eda855783c4b05ac986db72a659bad7caf2fea
Ralph Smith <rsmith@astralbrands.com>
2018-11-02 10:07:34 -0400

    co

--- app/code/local/Astral/Shipping/Model/Observer.php
+++ app/code/local/Astral/Shipping/Model/Observer.php
@@ -10,13 +10,13 @@ class Astral_Shipping_Model_Observer{
             $cc = $quote->getCouponCode();
             $sfs = false;
                 if(!isset($cc)||empty($cc)) {
-                $this->hideFreeShipping($carriers);
+                //$this->hideFreeShipping($carriers);
                 }elseif(isset($cc)&&!empty($cc)){
                 $oCoupon = Mage::getModel('salesrule/coupon')->load($quote->getCouponCode(), 'code');
                 $oRule = Mage::getModel('salesrule/rule')->load($oCoupon->getRuleId());
                 $sfs = $oRule->getData('simple_free_shipping');
                     if ($sfs == 0) {
-                        $this->hideFreeShipping($carriers);
+                        //$this->hideFreeShipping($carriers);
                     }
                 }
             }
