bd9082f29f44ce0cabd4ea00bd96e80cd0dfda51
Ralph Smith <rsmith@astralbrands.com>
2018-11-02 11:18:46 -0400

    upd

--- app/code/local/Astral/Payments/Model/Observer.php
+++ app/code/local/Astral/Payments/Model/Observer.php
@@ -1,7 +1,8 @@
 <?php
 class Astral_Payments_Model_Observer{
-    public function addPaymentMethods( Varien_Event_Observer $observer ){
-
+    public function addPaymentMethod( Varien_Event_Observer $observer ){
+        Mage::log(__METHOD__);
+        Mage::log($observer->getEvent()->getMethodInstance()->getCode());
     }
 
 }
