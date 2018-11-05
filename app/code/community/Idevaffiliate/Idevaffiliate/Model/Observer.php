80cd14eb73b54534376c6e6b3decfaba6a7492ae
Ralph Smith <rsmith@astralbrands.com>
2018-11-05 09:15:17 -0500

    upg

--- app/code/community/Idevaffiliate/Idevaffiliate/Model/Observer.php
+++ app/code/community/Idevaffiliate/Idevaffiliate/Model/Observer.php
@@ -4,7 +4,7 @@ class Idevaffiliate_Idevaffiliate_Model_Observer
 			public function track(Varien_Event_Observer $observer)
 			{
 				$orderIds = $observer->getData('order_ids');
-				foreach($orderIds as $_orderId){	
+				foreach($orderIds as $_orderId){
 					$order_details = Mage::getModel('sales/order')->load($_orderId);
 					$idev_subtotal = $order_details->base_subtotal; 
 					$idev_discount = $order_details->base_discount_amount;
@@ -12,18 +12,33 @@ class Idevaffiliate_Idevaffiliate_Model_Observer
 					$coupon_code = $order_details->coupon_code;
 					$items = $order_details->getAllVisibleItems();
 					$skus = array();
-					foreach($items as $i):
-						$skus[] = $i->getSku();
-					endforeach;
-					$products_purchased = implode('|', $skus);
-					$tracking_url = Mage::getStoreConfig('idevaffiliate/idevaffiliate/idev_tracking_url') . 'sale.php';
-					$tracking_fields = 'profile=54&ip_address='.$_SERVER['REMOTE_ADDR'].'&idev_saleamt='.$idev_saleamt.'&idev_ordernum='.$_orderId.'&products_purchased='.$products_purchased.'&coupon_code='.$coupon_code;
-					Mage::log($tracking_url."/".$tracking_fields, false, 'idev_conversion_log_'.date('Y-m-d').'.log');
+					    foreach($items as $i) {
+                        $skus[] = $i->getSku();
+                        }
+                        if(isset($_SERVER['X-Forwarded-For'])){
+                        $ip = $_SERVER['X-Forwarded-For'];
+                        }else{
+                        $ip = $_SERVER['REMOTE_ADDR'];
+                        }
+                    $products_purchased = implode('|', $skus);
+                    $tracking_url = Mage::getStoreConfig('idevaffiliate/idevaffiliate/idev_tracking_url') . 'sale.php';
+                    $data = array();
+					$data['profile'] = '54';
+                    $data['ip_address'] = $ip;
+                    $data['idev_saleamt'] = $idev_saleamt;
+                    $data['idev_ordernum'] = $_orderId;
+                    $data['products_purchased'] = $products_purchased;
+                    $data['coupon_code'] = $coupon_code;
+                    $query = http_build_query($data);
+					Mage::log($tracking_url, false, 'idev_conversion_log_'.date('Y-m-d').'.log');
+                    Mage::log($query, false, 'idev_conversion_log_'.date('Y-m-d').'.log');
 					$ch = curl_init();
-					curl_setopt($ch, CURLOPT_URL, $tracking_url."/".$tracking_fields);
+					curl_setopt($ch, CURLOPT_URL, $tracking_url);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 					$return = curl_exec($ch);
 					curl_close($ch);
 				}
 			}
+
 }
