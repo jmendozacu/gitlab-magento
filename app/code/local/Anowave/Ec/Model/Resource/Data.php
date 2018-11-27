16c4178202d62f16823b7ce25e2475c3a766fcef
jiadams <joshua.ian.adams@gmail.com>
2018-11-27 11:57:15 -0500

    Init Commit

--- app/code/local/Anowave/Ec/Model/Resource/Data.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Anowave_Ec_Model_Resource_Data extends Mage_Core_Model_Resource_Db_Abstract
-{
-	protected function _construct()
-    {
-        $this->_init('ec/data','data_id');
-    }
-}
--- app/code/local/Anowave/Ec/Model/Resource/Store.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Anowave_Ec_Model_Resource_Store extends Mage_Core_Model_Resource_Db_Abstract
-{
-	protected function _construct()
-    {
-        $this->_init('ec/store','ab_primary_id');
-    }
-}
