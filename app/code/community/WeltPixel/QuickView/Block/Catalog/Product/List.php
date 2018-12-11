b23ecf77bed2e24f0e980bee4ca118325b9342e0
jiadams <joshua.ian.adams@gmail.com>
2018-12-11 08:34:34 -0500

    Using Get Data

--- app/code/community/WeltPixel/QuickView/Block/Catalog/Product/List.php
+++ app/code/community/WeltPixel/QuickView/Block/Catalog/Product/List.php
@@ -3,49 +3,6 @@
 class WeltPixel_QuickView_Block_Catalog_Product_List extends Mage_Catalog_Block_Product_List
 {
 
-    protected function _getProductCollection()
-    {
-        if (is_null($this->_productCollection)) {
-            $layer = $this->getLayer();
-            /* @var $layer Mage_Catalog_Model_Layer */
-            if ($this->getShowRootCategory()) {
-                $this->setCategoryId(Mage::app()->getStore()->getRootCategoryId());
-            }
-
-            if (Mage::registry('product')) {
-                /** @var Mage_Catalog_Model_Resource_Category_Collection $categories */
-                $categories = Mage::registry('product')->getCategoryCollection()
-                    ->setPage(1, 1)
-                    ->load();
-                if ($categories->count()) {
-                    $this->setCategoryId($categories->getFirstItem()->getId());
-                }
-            }
-
-            $origCategory = null;
-            if ($this->getCategoryId()) {
-                $category = Mage::getModel('catalog/category')
-                    ->addAttributeToSelect('*')
-                    ->addAttributeToFilter('alt_image',true)
-                    ->load($this->getCategoryId());
-                if ($category->getId()) {
-                    $origCategory = $layer->getCurrentCategory();
-                    $layer->setCurrentCategory($category);
-                    $this->addModelTags($category);
-                }
-            }
-            $this->_productCollection = $layer->getProductCollection();
-
-            $this->prepareSortableFieldsByCategory($layer->getCurrentCategory());
-
-            if ($origCategory) {
-                $layer->setCurrentCategory($origCategory);
-            }
-        }
-
-        return $this->_productCollection;
-    }
-
     public function getQuickViewUrl($product, $additional = array()) {
         return Mage::helper('weltpixel_quickview')->getProductUrl($product, $additional);
     }
