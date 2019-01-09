8b939a6d6a08219d4e036dec95c05e6901b6d767
jiadams <joshua.ian.adams@gmail.com>
2019-01-09 16:22:43 -0500

    Logging

--- app/code/local/Astral/Integrations/Helper/CartDecorator.php
+++ app/code/local/Astral/Integrations/Helper/CartDecorator.php
@@ -40,8 +40,13 @@ class Astral_Integrations_Helper_CartDecorator extends Mage_Core_Helper_Abstract
 
             foreach($cart->getAllVisibleItems() as $item) {
                 //Get Contents
+                $isBundled = $item->getTypeId() == 
+                Mage_Catalog_Model_Product_Type::TYPE_BUNDLE;
+
+                Mage::log($isBundled, null,'astral_integrations.log');
+
                 $row = array();
-                $row['id'] = $item->getData('sku');
+                $row['id'] = $item->getSku();
                 $row['price'] = number_format($item->getPrice(), 2);
                 $row['quantity'] = $item->getQty();
                 $pixelCart['contents'][] = $row;
