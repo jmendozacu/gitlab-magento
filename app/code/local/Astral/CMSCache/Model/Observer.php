448c289b2fff13a0e5b7c86ae926ad03eea14d4a
Ralph Smith <rsmith@astralbrands.com>
2019-01-09 09:57:35 -0500

    upd

--- app/code/local/Astral/CMSCache/Model/Observer.php
+++ app/code/local/Astral/CMSCache/Model/Observer.php
@@ -1,45 +1,6 @@
 <?php
-/**
- * Rkt_SbCache extension
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the MIT License
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/mit-license.php
- *
- * @category       Rkt
- * @package        Rkt_SbCache
- * @author         Rajeev K Tomy <programmer.rkt@gmail.com>
- * @copyright      Copyright (c) 2015
- * @license        http://opensource.org/licenses/mit-license.php MIT License
- */
-
-/**
- * Observer Class
- *
- * Listen To : core_block_abstract_to_html_before
- *
- * Use to apply cacheing for CMS Blocks, which lacks in core Magento.
- *
- * @category    Rkt
- * @package     Rkt_Config
- * @author      Rajeev K Tomy <programmer.rkt@gmail.com>
- */
 class Astral_CMSCache_Model_Observer
 {
-
-	/**
-	 * Use to apply cacheing for CMS Blocks in Magento.
-	 *
-	 * By default, Magento is not applying cacheing for CMS blocks. This function
-	 * will apply cache for CMS Blocks and thus help us to overcome this difficulty.
-	 *
-	 * @access public
-	 * @param  Varien_Event_Observer       $observer
-	 * @return Rkt_SbCache_Model_Observer
-	 */
 	public function enableCmsBlockCaching(Varien_Event_Observer $observer)
 	{
         if (!$this->_getHelper()->isEnabled()){
@@ -52,7 +13,6 @@ class Astral_CMSCache_Model_Observer
         if ($block instanceof Mage_Cms_Block_Widget_Block
             || $block instanceof Mage_Cms_Block_Block
         ) {
-
         	//making a unique cache key for each cms blocks so that cached HTML
         	//content will be unique per each static block
             $cacheKeyData = array(
@@ -65,7 +25,6 @@ class Astral_CMSCache_Model_Observer
                 //Mage::helper('rkt_sbcache')->randomString() // UNCOMMENT IF IT IS NECESSARY
             );
             $block->setCacheKey(implode('_', $cacheKeyData));
-
             //set cache tags. This will help us to clear the cache related to
             //a static block based on store, CMS cache, or by identifier.
             $block->setCacheTags(array(
@@ -73,14 +32,12 @@ class Astral_CMSCache_Model_Observer
 		        Mage_Cms_Model_Block::CACHE_TAG,
 		        (string)$block->getBlockId()
 		    ));
-
 		    //setting cache life time to default. ie 7200 seconds(2 hrs).
 		    //an integer value in seconds. eg : 86400 for one day cache
             $block->setCacheLifetime($this->_getHelper()->getCacheTimeout());
         }
 
 	}
-
     /**
      * @return Rkt_SbCache_Helper_Data
      */
@@ -88,5 +45,4 @@ class Astral_CMSCache_Model_Observer
     {
         return Mage::helper('rkt_sbcache');
     }
-
 }
