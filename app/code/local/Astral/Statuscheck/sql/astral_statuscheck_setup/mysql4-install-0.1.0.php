a16d117ee6584456919b8e09377b9a2ced191e3b
Ralph Smith <rsmith@astralbrands.com>
2019-01-30 22:05:00 -0500

    setup fixes

--- app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
+++ app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
@@ -1,9 +1,8 @@
 <?php
 $this->startSetup();
-$this->register();
 $this->run("
-DROP TABLE IF EXISTS `{$this->getTable('astral_statuscheck')}`;
-CREATE TABLE IF NOT EXISTS `{$this->getTable('astral_statuscheck')}` (
+DROP TABLE IF EXISTS `{$this->getTable('astral_statuscheck_scc')}`;
+CREATE TABLE IF NOT EXISTS `{$this->getTable('astral_statuscheck_scc')}` (
 	`sc_id` int(10) unsigned NOT NULL auto_increment,
 	`case_id` int(10) unsigned NOT NULL,
 	`check_count` int(1) unsigned NOT NULL,
