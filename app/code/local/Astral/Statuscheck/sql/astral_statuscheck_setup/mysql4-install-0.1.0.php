5b4ff04e474a67c3dbcfe8c1fda50851846e8022
Ralph Smith <rsmith@astralbrands.com>
2019-01-30 22:05:38 -0500

    bad key

--- app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
+++ app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `{$this->getTable('astral_statuscheck_scc')}` (
 	`case_id` int(10) unsigned NOT NULL,
 	`check_count` int(1) unsigned NOT NULL,
 	`bypass_score` int(1) unsigned NOT NULL,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`sc_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
 $this->endSetup();
