d870b3ae133f8c2bde687bd9898647597f3c57fc
Ralph Smith <rsmith@astralbrands.com>
2019-02-01 10:53:22 -0500

    Moving increment id

--- app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
+++ app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
@@ -3,10 +3,11 @@ $this->startSetup();
 $this->run("
 DROP TABLE IF EXISTS `{$this->getTable('astral_statuscheck_scc')}`;
 CREATE TABLE IF NOT EXISTS `{$this->getTable('astral_statuscheck_scc')}` (
+	`id` int(10) unsigned NOT NULL AUTOINCREMENT,
 	`increment_id` int(10) unsigned NOT NULL,
 	`check_count` int(1) unsigned NOT NULL DEFAULT '0',
 	`bypass_score` int(1) unsigned NOT NULL DEFAULT '0',
-	PRIMARY KEY (`increment_id`)
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
 $this->endSetup();
