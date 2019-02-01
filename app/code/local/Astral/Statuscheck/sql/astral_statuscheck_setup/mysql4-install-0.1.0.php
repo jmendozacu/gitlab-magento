678f12093b201fcefd7cd5a8503fd437d0e3a347
Ralph Smith <rsmith@astralbrands.com>
2019-02-01 11:05:13 -0500

    SQL Update

--- app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
+++ app/code/local/Astral/Statuscheck/sql/astral_statuscheck_setup/mysql4-install-0.1.0.php
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `{$this->getTable('astral_statuscheck_scc')}` (
 	`increment_id` varchar(255) NOT NULL,
 	`check_count` int(1) unsigned NOT NULL DEFAULT '0',
 	`bypass_score` int(1) unsigned NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`increment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
 $this->endSetup();
