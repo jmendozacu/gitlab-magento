8373ec180b4645f70efec75ad720634338e28121
Ralph Smith <rsmith@astralbrands.com>
2019-02-12 09:43:20 -0500

    More cleanup

--- app/code/local/Born/Sagelog/Model/Mysql4/Logging.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Born_Sagelog_Model_Mysql4_Logging extends Mage_Core_Model_Mysql4_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init("sagelog/logging", "entity_id");
-    }
-}
